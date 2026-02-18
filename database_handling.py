42f7f5e645a29d45654a5f3ce3e6883e04b6699c
Middlewarer | Grabchuk Alex <mranarchy@mail.ru>
2026-02-18 03:53:22 +0300

    Database | user | session simple_db created

---
+++ database_handling.py
@@ -0,0 +1,49 @@
+import sqlite3
+from datetime import datetime
+import os
+
+#env-before-after
+
+DB_NAME = 'tracker_database.db'
+
+print('Создаем БД')
+
+conn = sqlite3.connect(DB_NAME)
+cursor = conn.cursor()
+
+
+#User Table Creation
+cursor.execute('''CREATE TABLE IF NOT EXISTS user
+
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                status INTEGER NOT NULL ,
+                name TEXT NOT NULL UNIQUE,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL)
+                ''')
+
+print('Инициализация user успешна')
+#Session Table Creation
+cursor.execute('''CREATE TABLE IF NOT EXISTS session (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                title TEXT NOT NULL,
+                time_in_mins INTEGER NOT NULL,
+                created_at TIMESTAMP NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES user(id) )
+''')
+
+print('Инициализация session успешна')
+
+
+def create_user(status, name, username, password):
+    global cursor
+    cursor.execute(f'''INSERT INTO user (status, name, username, password) VALUES (?, ?, ?, ?)''', (status, name, username, password))
+    cursor.connection.commit()
+
+def create_session(title: str, time_in_mins: int, user_id: int) -> None:
+    global cursor
+    created_at = datetime.now()
+    cursor.execute('''INSERT INTO session (user_id, title, time_in_mins, created_at) VALUES (?, ?, ?, ?)''', (user_id, title, time_in_mins, created_at))
+    cursor.connection.commit()
+
